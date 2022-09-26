57c94411532ddcd03c23381fb85bfd1b557020ef
Matthew Wong <matthewwong@Matthews-MacBook-Air.local>
2022-09-26 15:52:47 -0400

    minor updates

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,11 +1,12 @@
 import { useContext } from "react";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import AuthContext from "../context/AuthContext";
 import { signInWithGoogle, signOut } from "../firebaseConfig";
 import "./Header.css";
 
 const Header = () => {
   const { user } = useContext(AuthContext);
+  const navigate = useNavigate();
 
   return (
     <header className="Header">
--- src/components/LandingPage.tsx
+++ src/components/LandingPage.tsx
@@ -1,6 +1,15 @@
+import { useContext, useEffect } from "react";
+import { useNavigate } from "react-router-dom";
+import AuthContext from "../context/AuthContext";
 import "./LandingPage.css";
 
 const LandingPage = () => {
+  const { currentUserProfile } = useContext(AuthContext);
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    currentUserProfile && navigate("/overview");
+  }, [currentUserProfile]);
   return <div className="LandingPage">LandingPage works</div>;
 };
 
